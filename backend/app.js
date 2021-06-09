c3f1d13c7c44eddb9511ad2c15ccc52c1d7fd557
Ahnaf <ahnaf.24csedu.039@gmail.com>
2021-06-09 21:37:32 +0600

    [backend] installing required packeges

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("config");
+const morgan = require('morgan');
+const cors = require('cors');
+const compression = require('compression');
+const mongoose = require('mongoose');
+
 const PORT = config.get("DEV_BACKEND_PORT");
+const dbAddress = config.get('MONGODB_SERVER');
 
 const app = express();
+
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+mongoose
+  .connect(dbAddress, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.log('MongoDB Connection Failed!', err.message));
 
 const server = app.listen(process.env.PORT || PORT, async () => {
   console.log(`server started on port ${PORT}`);
 });
+
+app.use('/', (req, res) => {
+  res.send('YO!');
+})
