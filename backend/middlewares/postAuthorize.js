e42e2044924a4da465457910b97debc71ebe605e
Ahnaf <ahnaf.24csedu.039@gmail.com>
2021-06-12 12:49:43 +0600

    fixed post authorization rules

--- backend/middlewares/postAuthorize.js
+++ backend/middlewares/postAuthorize.js
@@ -4,10 +4,30 @@ module.exports = async (req, res, next) => {
   const postId = req.params.postId;
   const userId = req.user._id;
   try {
-    const post = await Post.findById(postId);
-    console.log(post.userId, userId);
-    if (post.userId == userId) next();
-    else return res.status(400).send("You can only edit your posts");
+    if (postId) {
+      const post = await Post.findById(postId);
+      if (post.userId == userId) {
+        if (req.body.comments) {
+          return res
+            .status(400)
+            .send(
+              "Use /api/posts/:postId/comments/:commentId for updating comments"
+            );
+        } else {
+          next();
+        }
+      } else return res.status(400).send("You can only edit your posts");
+    } else {
+      if (req.body.comments) {
+        return res
+          .status(400)
+          .send(
+            "Use /api/posts/:postId/comments/:commentId for updating comments"
+          );
+      } else {
+        next();
+      }
+    }
   } catch (err) {
     return res.status(400).send(err);
   }
