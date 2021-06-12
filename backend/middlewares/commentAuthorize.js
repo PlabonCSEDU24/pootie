0e8e606b2dc2e8a153ff3908155e1cf506f6f747
Ahnaf <ahnaf.24csedu.039@gmail.com>
2021-06-12 12:48:15 +0600

    new model and authorization for comments

--- backend/middlewares/commentAuthorize.js
+++ backend/middlewares/commentAuthorize.js
@@ -1,32 +1,16 @@
 const { Post } = require("../models/posts");
+const { Comment } = require("../models/comments");
 const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = async (req, res, next) => {
-  const data = await Post.find(
-    {
-      $and: [
-        { _id: ObjectId(req.params.postId) },
-        { "comments._id": ObjectId(req.params.commentId) },
-      ],
-    },
-    {
-      comments: {
-        $elemMatch: { _id: ObjectId(req.params.commentId) },
-      },
-      _id: 0,
-    }
-  );
-  const [
-    {
-      ["comments"]: [ret],
-    },
-  ] = data;
-  const commentatorId = ret.commentatorId;
   const userId = req.user._id;
   try {
-    if (userId === commentatorId) next();
-    else return res.status(400).send("Can not edit other people's comment!")
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment)
+      res.status(410).send("This comment doesn't exist in database!");
+    if (userId === comment.userId.toString()) next();
+    else return res.status(401).send("Can not edit other people's comment!");
   } catch (error) {
-    return res.status(400).send(error);
+    return res.status(400).send('Something went wrong with database');
   }
-}
+};
